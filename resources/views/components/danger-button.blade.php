4cbcf4056c13527b27ece1caec64307fa1b0c037
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-05-05 12:28:41 -0300

    pequena alteração de design

--- resources/views/components/danger-button.blade.php
+++ resources/views/components/danger-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'h-10 px-4 flex items-center justify-center bg-red-500 hover:bg-red-600 duration-300 transition text-sm font-semibold text-white rounded-lg']) }}>
     {{ $slot }}
 </button>
