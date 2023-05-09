5fd35a058e8061ed4d2a4ead28bf37695996f10c
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-05-09 14:51:38 -0300

    implementação do componente para editar usuário

--- app/Http/Livewire/DepartmentEdit.php
+++ app/Http/Livewire/DepartmentEdit.php
@@ -2,12 +2,60 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use App\Models\Department;
 
 class DepartmentEdit extends Component
 {
+    public int $department_id;
+    public string $name;
+    public array $agents = [];
+
+    public function mount($id)
+    {
+        $department = Department::with('agents:id')->where('id', $id)->first([
+            'departments.id',
+            'departments.name',
+        ]);
+
+        $this->department_id = $department->id;
+        $this->name = $department->name;
+        $this->agents = $department->agents->modelKeys();
+    }
+
     public function render()
     {
-        return view('livewire.department-edit');
+        $agents = User::select(['id', 'name', 'email', 'avatar'])
+            ->whereHas("roles", fn($q) => $q->where("name", "agent"))
+            ->get();
+
+        return view('livewire.department-edit', ['all_agents' => $agents]);
     }
+
+    public function add_agent(int $agent_id)
+    {
+        if(in_array($agent_id, $this->agents)) {
+            $pos = array_search($agent_id, $this->agents);
+            array_splice($this->agents, $pos, 1);
+        } else {
+            $this->agents[] = $agent_id;
+        }
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'agents' => 'array|exists:users,id',
+            'agents.*' => 'int'
+        ]);
+
+        $department = Department::find($this->department_id);
+        $department->update(['name' => $this->name]);
+        $department->agents()->sync($this->agents);
+
+        session()->flash('status', 'Departamento atualizado.');
+    }
+
 }
