8536d7523d63cd3d43180fdeab89dbfb52ab2174
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-05-09 12:41:17 -0300

    pequena alteração de design

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'h-10 px-4 flex items-center justify-center bg-primary-500 hover:bg-primary-600 duration-300 transition text-sm font-semibold text-white rounded-lg']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'h-10 px-4 gap-1 flex items-center justify-center bg-primary-500 hover:bg-primary-600 duration-300 transition text-sm font-semibold text-white rounded-md uppercase']) }}>
     {{ $slot }}
 </button>
