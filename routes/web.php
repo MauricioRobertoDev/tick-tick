edb7a492f9fc86d7f6551da75b6c4f3ee151383b
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-05-10 15:21:59 -0300

    criação das rotas para listar, criar e editar usuários

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Livewire\DepartmentCreate;
-use App\Http\Livewire\DepartmentEdit;
-use App\Http\Livewire\DepartmentIndex;
 use App\Http\Livewire\TagList;
+use App\Http\Livewire\UserEdit;
+use App\Http\Livewire\UserIndex;
+use App\Http\Livewire\UserCreate;
+use App\Http\Livewire\DepartmentEdit;
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\DepartmentIndex;
+use App\Http\Livewire\DepartmentCreate;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,9 +37,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/tags', TagList::class)->name('tag.index');
+
     Route::get('/departamentos', DepartmentIndex::class)->name('department.index');
     Route::get('/departamentos/novo', DepartmentCreate::class)->name('department.create');
     Route::get('/departamentos/editar/{id}', DepartmentEdit::class)->name('department.edit');
+
+    Route::get('/usuarios', UserIndex::class)->name('user.index');
+    Route::get('/usuarios/novo', UserCreate::class)->name('user.create');
+    Route::get('/usuarios/editar/{id}', UserEdit::class)->name('user.edit');
 });
 
 require __DIR__.'/auth.php';
