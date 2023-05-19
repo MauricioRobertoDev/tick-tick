3fa2099998e1a53f7f1774ae398ec3e7acfa9c2a
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-05-19 13:44:52 -0300

    adicição de uma série de rotas para listar, ver e criar tickets da perspectiva do usuário

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,18 @@ use App\Models\GameMatch;
 use App\Http\Livewire\TagList;
 use App\Http\Livewire\UserEdit;
 use App\Http\Livewire\UserIndex;
+use App\Http\Livewire\TicketEdit;
 use App\Http\Livewire\UserCreate;
+use App\Http\Livewire\TicketIndex;
+use App\Http\Livewire\TicketCreate;
+use App\Http\Livewire\MyTicketIndex;
 use App\Http\Livewire\DepartmentEdit;
+use App\Http\Livewire\MyTicketCreate;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\DepartmentIndex;
 use App\Http\Livewire\DepartmentCreate;
 use App\Http\Controllers\ProfileController;
-use App\Http\Livewire\TicketCreate;
-use App\Http\Livewire\TicketEdit;
-use App\Http\Livewire\TicketIndex;
+use App\Http\Livewire\MyTicketShow;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,19 +39,25 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/tags', TagList::class)->name('tag.index');
+    Route::group(['middleware' => ['role:admin|agent']], function () {
+        Route::get('/tickets', TicketIndex::class)->name('ticket.index');
+    });
 
-    Route::get('/departamentos', DepartmentIndex::class)->name('department.index');
-    Route::get('/departamentos/novo', DepartmentCreate::class)->name('department.create');
-    Route::get('/departamentos/editar/{id}', DepartmentEdit::class)->name('department.edit');
+     Route::group(['middleware' => ['role:admin']], function () {
+        Route::get('/tags', TagList::class)->name('tag.index');
 
-    Route::get('/usuarios', UserIndex::class)->name('user.index');
-    Route::get('/usuarios/novo', UserCreate::class)->name('user.create');
-    Route::get('/usuarios/editar/{id}', UserEdit::class)->name('user.edit');
+        Route::get('/departamentos', DepartmentIndex::class)->name('department.index');
+        Route::get('/departamentos/novo', DepartmentCreate::class)->name('department.create');
+        Route::get('/departamentos/editar/{id}', DepartmentEdit::class)->name('department.edit');
 
-    Route::get('/tickets', TicketIndex::class)->name('ticket.index');
-    Route::get('/tickets/novo', TicketCreate::class)->name('ticket.create');
-    Route::get('/tickets/editar/{id}', TicketEdit::class)->name('ticket.edit');
+        Route::get('/usuarios', UserIndex::class)->name('user.index');
+        Route::get('/usuarios/novo', UserCreate::class)->name('user.create');
+        Route::get('/usuarios/editar/{id}', UserEdit::class)->name('user.edit');
+    });
+
+    Route::get('/meus-tickets', MyTicketIndex::class)->name('myticket.index');
+    Route::get('/meus-tickets/novo', MyTicketCreate::class)->name('myticket.create');
+    Route::get('/meus-tickets/{id}', MyTicketShow::class)->name('myticket.show');
 });
 
 require __DIR__.'/auth.php';
