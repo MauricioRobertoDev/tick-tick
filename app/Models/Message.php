9c6a2c35161dfab67af971ee53d7c4448fcc25e1
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-05-19 13:39:55 -0300

    criação do model e organização dos seus relacionamentos

---
+++ app/Models/Message.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Ticket;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'user_id',
+        'ticket_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+}
